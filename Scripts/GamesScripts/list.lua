aef9bd07c33c25b8f6ad2508705bfa07043143a6
fixingbugsat3am-dot <fixingbugsat3am@gmail.com>
2026-05-09 21:18:01 -0400

    Update list.lua

--- Scripts/GamesScripts/list.lua
+++ Scripts/GamesScripts/list.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [83321986508238] = "", -- Brainrot Clicker
+  [83321986508238] = "loadstring(game:HttpGet("https://raw.githubusercontent.com/fixingbugsat3am-dot/loadstringg/main/Scripts/GamesScripts/BrainrotClicker.lua"))()", -- Brainrot Clicker
 
 }
 
