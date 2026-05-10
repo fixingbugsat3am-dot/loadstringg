13c202453c1a48f17221b8ae212c01233b121ce7
fixingbugsat3am-dot <fixingbugsat3am@gmail.com>
2026-05-09 21:49:34 -0400

    Update list.lua

--- Scripts/GamesScripts/list.lua
+++ Scripts/GamesScripts/list.lua
@@ -1,6 +1,13 @@
 local Games = {
-  [83321986508238] = "https://raw.githubusercontent.com/fixingbugsat3am-dot/loadstringg/main/Scripts/GamesScripts/BrainrotClicker.lua", -- Brainrot Clicker
-
+    [83321986508238] = {
+        Name = "Brainrot Clicker",
+        URL  = "https://raw.githubusercontent.com/fixingbugsat3am-dot/loadstringg/main/Scripts/GamesScripts/BrainrotClicker.lua",
+    },
+    -- add more games here:
+    -- [GAMEID] = {
+    --     Name = "Game Name",
+    --     URL  = "https://raw.githubusercontent.com/...",
+    -- },
 }
 
 return Games
