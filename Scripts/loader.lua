95c41fda0c0383372ca54cedbbf41bb42b77ca39
fixingbugsat3am-dot <fixingbugsat3am@gmail.com>
2026-05-09 20:52:49 -0400

    Initial commit

---
+++ Scripts/loader.lua
@@ -0,0 +1,11 @@
+local HttpGet = game.HttpGet
+local GameId: number = game.GameId
+
+local Games: {[number]: string} = loadstring(
+  HttpGet(game, "")
+)() :: any
+
+local URL: string? = Games[GameId]
+if not URL then return end
+
+loadstring(HttpGet(game, URL))()
